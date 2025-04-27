d5b451f872b3d7adebce0a3aeb9555b69149e06c
Khon Maxim <115972832+Kvan6Tuk4Manko@users.noreply.github.com>
2025-04-27 13:25:01 +0300

    Update levels.php

--- levels.php
+++ levels.php
@@ -6,6 +6,11 @@ body {
     background: url("https://kartinkin.net/uploads/posts/2021-04/thumbs/1618049829_11-p-molniya-zamok-fentezi-12.jpg"); 
     background-size: 100% 100%;
 }
+    
+.hcolor {
+color: #FFFFFF;
+font-size: 40px;
+}
 
 </body>
 </html>
