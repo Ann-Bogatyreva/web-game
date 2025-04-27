e72bc7540f8575c5307603b9d9e9277c14639f76
Khon Maxim <115972832+Kvan6Tuk4Manko@users.noreply.github.com>
2025-04-27 13:24:17 +0300

    Create levels.php

---
+++ levels.php
@@ -0,0 +1,11 @@
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+
+<style>
+
+body {
+    background: url("https://kartinkin.net/uploads/posts/2021-04/thumbs/1618049829_11-p-molniya-zamok-fentezi-12.jpg"); 
+    background-size: 100% 100%;
+}
+
+</body>
+</html>
