6a2004391efdabfe8e6d90112efebac9a32c5da6
Khon Maxim <115972832+Kvan6Tuk4Manko@users.noreply.github.com>
2025-04-27 13:29:06 +0300

    Update levels.php

--- levels.php
+++ levels.php
@@ -12,5 +12,37 @@ color: #FFFFFF;
 font-size: 40px;
 }
 
+.btn {
+    display: inline-block; 
+    box-sizing: border-box;
+    padding: 0 25px;
+    margin: 0 15px 15px 0;
+    outline: none;
+    border: 1px solid #fff;
+    border-radius: 50px;
+    height: 46px;
+    line-height: 46px;
+    font-size: 25px;
+    font-weight: 600;
+    text-decoration: none;
+    color: #9933CC;
+    background-color: #66FFCC;
+    box-shadow: 0 10px 6px rgb(65 132 144 / 0%), 0 1px 3px rgb(0 0 0 / 10%);
+    cursor: pointer;
+    user-select: none;
+    appearance: none;
+    touch-action: manipulation; 
+    vertical-align: top;
+    transition: box-shadow 0.2s;
+    width: 250px;
+}
+
+.btn:focus-visible {
+    border: 1px solid #4c51f9;
+    outline: none;
+}
+    
+</style>
+    
 </body>
 </html>
