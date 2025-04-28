f27b524dda159c37869c473420ee0cae1bc0aaba
Котя <hinokokurushima@gmail.com>
2025-04-28 09:16:08 +0300

    Create options.php

---
+++ options.php
@@ -0,0 +1,24 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title> Настройки </title>
+</head>
+
+<body
+    style="
+    background-color: rgb(139, 201, 182)">
+
+<body>
+
+    <front margin: 0px 0px 0px 10px>
+
+    <h2>
+         Мы только начинаем свой путь программирования, так что игра простенькая. Жалобы и предложения можно написать на почту venerologbogat@mail.ru 
+    </h2>
+        </front>
+
+
+</body>
+</html>
