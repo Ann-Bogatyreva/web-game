19a8f79cc809717786858c40e43eb35ac3b81890
Котя <hinokokurushima@gmail.com>
2025-04-28 09:45:11 +0300

    Update jquery.snap-puzzle.js

--- jquery.snap-puzzle.js
+++ jquery.snap-puzzle.js
@@ -86,7 +86,7 @@ maxY = pile.height() - piece Height,
 
                             });
 
-                        } else {
+                        } else  {
 
                             // placed anywhere else
 
@@ -105,3 +105,144 @@ maxY = pile.height() - piece Height,
                                 backgroundPosition: (-y*pieceWidth)+'px '+(-x*pieceHeight)+'px',
 
                                 backgroundSize: that.width()
+  });
+
+                        }
+
+                    });
+
+                }
+
+            });
+
+            return this;
+
+        }
+
+
+
+        function init(that){
+
+            var puzzle_class = 'sp_'+new Date().getTime(),
+
+                puzzle = that.wrap('<span class="snappuzzle-wrap"/>').closest('span'),
+
+                src = that.attr('src'),
+
+                pieceWidth = that.width() / o.columns,
+
+                pieceHeight = that.height() / o.rows,
+
+                pile = $(o.pile).addClass('snappuzzle-pile'),
+
+                maxX = pile.width() - pieceWidth,
+
+                maxY = pile.height() - pieceHeight;
+
+
+
+            o.puzzle_class = puzzle_class;
+
+            that.data('options', o);
+
+
+
+            for (var x=0; x<o.rows; x++) {
+
+                for (var y=0; y<o.columns; y++) {
+
+                    $('<div class="snappuzzle-piece '+puzzle_class+'"/>').data('pos', x+'_'+y).css({
+
+                        width: pieceWidth,
+
+                        height: pieceHeight,
+
+                        position: 'absolute',
+
+                        left: Math.floor((Math.random()*(maxX+1))),
+
+                        top: Math.floor((Math.random()*(maxY+1))),
+
+                        zIndex: Math.floor((Math.random()*10)+1),
+
+                        backgroundImage: 'url('+src+')',
+
+                        backgroundPosition: (-y*pieceWidth)+'px '+(-x*pieceHeight)+'px',
+
+                        backgroundSize: that.width()
+
+                    }).draggable({
+
+                        start: function(e, ui){ $(this).removeData('slot'); },
+
+                        stack: '.snappuzzle-piece',
+
+                        containment: o.containment
+
+                    }).appendTo(pile).data('lastSlot', pile);
+
+
+
+                    $('<div class="snappuzzle-slot '+puzzle_class+'"/>').data('pos', x+'_'+y).css({
+
+                        width: pieceWidth,
+
+                        height: pieceHeight,
+
+                        left: y*pieceWidth,
+
+                        top: x*pieceHeight
+
+                    }).appendTo(puzzle).droppable({
+
+                        accept: '.'+puzzle_class,
+
+                        hoverClass: 'snappuzzle-slot-hover',
+
+                        drop: function(e, ui){
+
+                            var slot_pos = $(this).data('pos');
+
+
+
+                            // prevent dropping multiple pieces on one slot
+
+                            $('.snappuzzle-piece.'+puzzle_class).each(function(){
+
+                                if ($(this).data('slot') == slot_pos) slot_pos = false;
+
+                            });
+
+                            if (!slot_pos) return false;
+
+
+
+                            ui.draggable.data('lastSlot', $(this)).data('slot', slot_pos);
+
+                            ui.draggable.position({ of: $(this), my: 'left top', at: 'left top' });
+
+                            if (ui.draggable.data('pos')==slot_pos) {
+
+                                ui.draggable.addClass('correct');
+
+                                // fix piece
+
+                                // $(this).droppable('disable').fadeIn().fadeOut();
+
+                                $(this).droppable('disable').css('opacity', 1).fadeOut(1000);
+
+                                ui.draggable.css({opacity: 0, cursor: 'default'}).draggable('disable');
+
+                                if ($('.snappuzzle-piece.correct.'+puzzle_class).length == o.rows*o.columns) o.onComplete(that);
+
+                            }
+
+                        }
+
+                    });
+
+                }
+
+            }
+
+	    }
