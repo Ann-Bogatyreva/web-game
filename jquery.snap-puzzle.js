922e543819292151727f0637d79eff243fb7d1ff
Котя <hinokokurushima@gmail.com>
2025-04-28 09:32:05 +0300

    Create jquery.snap-puzzle.js

---
+++ jquery.snap-puzzle.js
@@ -0,0 +1,10 @@
+/*
+
+	jQuery snapPuzzle v1.0.0
+
+    Copyright (c) 2014 Hans Braxmeier / Simon Steinberger / Pixabay
+    GitHub: https://github.com/Pixabay/jQuery-snapPuzzle
+
+	License: http://www.opensource.org/licenses/mit-license.php
+
+*/
